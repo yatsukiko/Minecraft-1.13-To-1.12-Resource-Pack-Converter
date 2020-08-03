23616ebd587eabec7983817d7520db1e5a35b0fb
yatsukiko <yatsu@yatsukiko.me>
2020-08-03 21:27:52 +0100

    i goofed and uploaded wrong files

--- 7. edit pack.mcmeta.py
+++ 7. edit pack.mcmeta.py
@@ -1,3 +1,8 @@
-import os
-os.rename("assets/minecraft/textures/block", "assets/minecraft/textures/blocks")
-os.rename("assets/minecraft/textures/item", "assets/minecraft/textures/items")
+import fileinput
+from pathlib import Path
+
+print("Changing pack.mcmeta version.")
+
+with fileinput.FileInput("pack.mcmeta", inplace=True) as file:
+    for line in file:
+        print(line.replace('"pack_format": 4,', '"pack_format": 3,'), end='')
--- 8. delete cache.py
+++ 8. delete cache.py
@@ -1,2 +1,3 @@
-python3 -Bc "import pathlib; [p.unlink() for p in pathlib.Path('.').rglob('*.py[co]')]"
-python3 -Bc "import pathlib; [p.rmdir() for p in pathlib.Path('.').rglob('__pycache__')]"
+import pathlib; 
+[p.unlink() for p in pathlib.Path('.').rglob('*.py[co]')]
+[p.rmdir() for p in pathlib.Path('.').rglob('__pycache__')]
